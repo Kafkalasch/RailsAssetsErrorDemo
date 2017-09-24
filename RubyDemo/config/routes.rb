69c8b4490d0666b100ab6c246c545c6431a53357
Michael Höfler <Michi@Michaels-MacBook-Air.local>
2017-09-25 00:01:39 +0200

    Modifying to reproduce problem

--- RubyDemo/config/routes.rb
+++ RubyDemo/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'test_controller#index'
+  get 'test_controller/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
